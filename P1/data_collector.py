c272d4b358475acea2dbb3e53852d87b77303249
silviaferrer <silviaferrervaz@gmail.com>
2024-04-03 18:26:03 +0200

    starting to config hdfs

--- P1/data_collector.py
+++ P1/data_collector.py
@@ -2,10 +2,63 @@ import requests
 import os
 import io
 import pandas as pd
+from hdfs import InsecureClient
+from dotenv import load_dotenv
+
+def extract_open_data_bcn_income(urls):
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+
+    dfs = {}
+
+    for year in urls.keys():
+        df = extract_open_data_bcn_datasets(urls[year])
+        dfs[year+'_Distribucio_territorial_renda_familiar.parquet'] = df
+        # .to_csv(os.path.join(data_folder, year+'_Distribucio_territorial_renda_familiar.csv'))
+
+    return dfs
+
+def extract_open_data_bcn_elections(data_folder):    
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+
+    file_name = '2023_07_23_Eleccions_Congres_Diputats'
+
+    df = extract_open_data_bcn_datasets(url)
+    # .to_csv(os.path.join(data_folder, '2023_07_23_Eleccions_Congres_Diputats.csv'))
+
+    return {file_name, df}
+
+def extract_open_data_bcn_datasets(url):
+    response = requests.get(url)
+    df = pd.read_json(io.StringIO(response.content.decode('utf-8')))
+    df_result = pd.DataFrame(df.loc['records','result'])
+    return df_result
 
 
-def extract_open_data_bcn_income(data_folder):
-    urls = {'2017': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=e7206797-e57b-4ded-8c6c-62e9b4cb54f7',
+def create_hdfs(hdfs_host, hdfs_port, hdfs_user, temp_landing):
+    try:
+        client = InsecureClient(f'http://{hdfs_host}:{hdfs_port}', user=hdfs_user)
+        print(f"Connection to HDFS has been established successfully.")
+        if client.status(temp_landing, strict=False) is None:
+            client.makedirs(temp_landing)
+        
+        return client
+
+    except Exception as e:
+        client.close()
+        print(e)
+        return None
+
+
+def upload_file_hdfs(hdfs_dir, file_name, df):
+    print('hola')
+    #df.to_parquet(os.path.join(hdfs_dir, file_name))
+
+
+if __name__ == "__main__":
+    #data_folder = r"C:\Users\Silvia\OneDrive - Universitat Politècnica de Catalunya\Escritorio\UPC\MASTER DS\1B\BDM\P1\data"
+    urls_income = {'2017': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=e7206797-e57b-4ded-8c6c-62e9b4cb54f7',
             '2016': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=1d9ff171-6f23-45c1-b02f-203b0589f08a',
             '2015': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=bb4de997-cdf9-43ad-98c6-cc3a3e4d4f07',
             '2014': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=a741ff54-968e-4fa9-adfc-3635bc84b692',
@@ -16,35 +69,24 @@ def extract_open_data_bcn_income(data_folder):
             '2009': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=cdd351d9-eb5c-443b-a7be-bdadaf724614',
             '2008': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=2c178800-917e-4b59-9e4e-29da232fb26f',
             '2007': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=935b8e2f-996f-4829-8586-c7ddfcb9ba18'}
-    data_folder = data_folder + "/opendatabcn_income"
+    url_elections = "https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=e8fce35e-46b9-429e-a29f-945c33a3a8ef"
+    # load_dotenv()
+    temp_landing = "" #os.getenv('TEMPORAL_LANDING_DIR_PATH')
+    hdfs_host = "10.4.41.48" # os.getenv('HDFS_HBASE_HOST')
+    hdfs_port = "9870" #os.getenv('HDFS_PORT')
+    hdfs_user = "bdm" #os.getenv('HDFS_USER')
 
-    if not os.path.exists(data_folder):
-        os.makedirs(data_folder)
+    dict_income = extract_open_data_bcn_income(urls_income)
+    dict_election = extract_open_data_bcn_elections(url_elections)
 
-    for year in urls.keys():
-        extract_open_data_bcn_datasets(urls[year]).to_csv(os.path.join(data_folder, year+'_Distribucio_territorial_renda_familiar.csv'))
+    hdfs_client = create_hdfs(hdfs_host, hdfs_port, hdfs_user, temp_landing)
 
-def extract_open_data_bcn_elections(data_folder):
-    url = "https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=e8fce35e-46b9-429e-a29f-945c33a3a8ef"
-    data_folder = data_folder + "/opendatabcn_elections"
-    
-    if not os.path.exists(data_folder):
-        os.makedirs(data_folder)
-
-    extract_open_data_bcn_datasets(url).to_csv(os.path.join(data_folder, '2023_07_23_Eleccions_Congres_Diputats.csv'))
-
-def extract_open_data_bcn_datasets(url):
-    response = requests.get(url)
-    df = pd.read_json(io.StringIO(response.content.decode('utf-8')))
-    df_result = pd.DataFrame(df.loc['records','result'])
-    # print(df_result.head())
-    return df_result
+    if hdfs_client is not None:
+        for key, value in dict_income.items():
+            upload_file_hdfs(temp_landing, key, value)
 
 
-if __name__ == "__main__":
-    data_folder = r"C:\Users\Silvia\OneDrive - Universitat Politècnica de Catalunya\Escritorio\UPC\MASTER DS\1B\BDM\P1\data"
 
-    extract_open_data_bcn_income(data_folder)
-    extract_open_data_bcn_elections(data_folder)
+
 
 

a41dba559a6e6c7c96aa20617ce51f59599eb51c
silviaferrer <silviaferrervaz@gmail.com>
2024-04-06 19:12:51 +0200

    hbase connection failing

--- P1/data_collector.py
+++ P1/data_collector.py
@@ -4,6 +4,27 @@ import io
 import pandas as pd
 from hdfs import InsecureClient
 
+# Global variables
+LOCAL_DATA_FOLDER = "/data"
+URLS_INCOME = {'2017': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=e7206797-e57b-4ded-8c6c-62e9b4cb54f7',
+        '2016': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=1d9ff171-6f23-45c1-b02f-203b0589f08a',
+        '2015': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=bb4de997-cdf9-43ad-98c6-cc3a3e4d4f07',
+        '2014': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=a741ff54-968e-4fa9-adfc-3635bc84b692',
+        '2013': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=4ed48990-3686-4f28-8b1d-6a4350d91218',
+        '2012': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=336780a2-8a92-4356-9302-b88ed997e4a8',
+        '2011': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=c4751d23-fb9f-429a-be8b-1e119646417a',
+        '2010': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=1981aa37-2eba-4b55-948c-2264498a269f',
+        '2009': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=cdd351d9-eb5c-443b-a7be-bdadaf724614',
+        '2008': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=2c178800-917e-4b59-9e4e-29da232fb26f',
+        '2007': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=935b8e2f-996f-4829-8586-c7ddfcb9ba18'}
+URL_ELECTIONS = "https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=e8fce35e-46b9-429e-a29f-945c33a3a8ef"
+
+# Configuration HDFS
+TMP_LANDING_DIR = "/temporal_landing"
+HDFS_HOST = "10.4.41.48"
+HDFS_PORT = "9870"
+HDFS_USER = "bdm"
+
 def extract_open_data_bcn_income(data_folder, urls):
     try:   
         print('Starting Income dataset extraction') 
@@ -67,12 +88,12 @@ def extract_idealista(data_folder, source_dir):
         print(f"Error during extraction: {e}")
         return ''
     
-def create_hdfs(hdfs_host, hdfs_port, hdfs_user, temp_landing_dir):
+def create_hdfs(hdfs_host, hdfs_port, hdfs_user, tmp_landing_dir):
     try:
         client = InsecureClient(f'http://{hdfs_host}:{hdfs_port}', user=hdfs_user)
         print(f"Connection to HDFS has been established successfully.")
-        if client.status(temp_landing_dir, strict=False) is None:
-            client.makedirs(temp_landing_dir)
+        if client.status(tmp_landing_dir, strict=False) is None:
+            client.makedirs(tmp_landing_dir)
         
         return client
 
@@ -81,53 +102,32 @@ def create_hdfs(hdfs_host, hdfs_port, hdfs_user, temp_landing_dir):
         print(e)
         return None
 
-def upload_file_hdfs(client, temp_landing_dir, local_path, dataset):
+def upload_file_hdfs(client, tmp_landing_dir, local_path, dataset):
     try:        
         str = file_path.split('/')
         file_name = str[len(str)-1]
-        remote_path = client.upload(temp_landing_dir + dataset + file_name, local_path, overwrite=True)
+        remote_path = client.upload(tmp_landing_dir + dataset + file_name, local_path, overwrite=True)
         print(f"Uploaded correctly - {remote_path}")
 
     except Exception as e:
         print(f"Error {e} during the upload of the file {file_name}")
 
 if __name__ == "__main__":
-    # intialize variables
-    local_data_folder = "/data"
-    urls_income = {'2017': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=e7206797-e57b-4ded-8c6c-62e9b4cb54f7',
-            '2016': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=1d9ff171-6f23-45c1-b02f-203b0589f08a',
-            '2015': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=bb4de997-cdf9-43ad-98c6-cc3a3e4d4f07',
-            '2014': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=a741ff54-968e-4fa9-adfc-3635bc84b692',
-            '2013': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=4ed48990-3686-4f28-8b1d-6a4350d91218',
-            '2012': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=336780a2-8a92-4356-9302-b88ed997e4a8',
-            '2011': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=c4751d23-fb9f-429a-be8b-1e119646417a',
-            '2010': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=1981aa37-2eba-4b55-948c-2264498a269f',
-            '2009': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=cdd351d9-eb5c-443b-a7be-bdadaf724614',
-            '2008': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=2c178800-917e-4b59-9e4e-29da232fb26f',
-            '2007': 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=935b8e2f-996f-4829-8586-c7ddfcb9ba18'}
-    url_elections = "https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=e8fce35e-46b9-429e-a29f-945c33a3a8ef"
-
-    # intialize hdfs variables
-    temp_landing_dir = "/temporal_landing"
-    hdfs_host = "10.4.41.48"
-    hdfs_port = "9870"
-    hdfs_user = "bdm"
-
     # extract data
-    local_paths_income = extract_open_data_bcn_income(local_data_folder, urls_income)
-    local_path_elections = extract_open_data_bcn_elections(local_data_folder, url_elections)
-    local_path_idealista = extract_idealista(local_data_folder, '.' + local_data_folder + '/idealista_source/')
+    local_paths_income = extract_open_data_bcn_income(LOCAL_DATA_FOLDER, URLS_INCOME)
+    local_path_elections = extract_open_data_bcn_elections(LOCAL_DATA_FOLDER, URL_ELECTIONS)
+    local_path_idealista = extract_idealista(LOCAL_DATA_FOLDER, '.' + LOCAL_DATA_FOLDER + '/idealista_source/')
 
     # create hdfs client and makedir
-    hdfs_client = create_hdfs(hdfs_host, hdfs_port, hdfs_user, temp_landing_dir)
+    hdfs_client = create_hdfs(HDFS_HOST, HDFS_PORT, HDFS_USER, TMP_LANDING_DIR)
 
     # upload files
     if hdfs_client is not None:
         for file_path in local_paths_income:
-            upload_file_hdfs(hdfs_client, temp_landing_dir, os.path.join(local_data_folder + '/' + file_path), '/income/')
+            upload_file_hdfs(hdfs_client, TMP_LANDING_DIR, os.path.join(LOCAL_DATA_FOLDER + '/' + file_path), '/income/')
 
-        upload_file_hdfs(hdfs_client, temp_landing_dir, os.path.join(local_data_folder + '/' + local_path_elections), '/elections/')
+        upload_file_hdfs(hdfs_client, TMP_LANDING_DIR, os.path.join(LOCAL_DATA_FOLDER + '/' + local_path_elections), '/elections/')
 
         for file_path in local_path_idealista:
-            upload_file_hdfs(hdfs_client, temp_landing_dir, os.path.join(local_data_folder + '/' + file_path), '/idealista/')
+            upload_file_hdfs(hdfs_client, TMP_LANDING_DIR, os.path.join(LOCAL_DATA_FOLDER + '/' + file_path), '/idealista/')
 
---
+++ P1/persistence_loader.py
@@ -0,0 +1,57 @@
+import happybase
+from hdfs import InsecureClient
+import pyarrow.parquet as pq
+import io
+import time
+
+# Configuration HBase
+HBASE_COLUMN_FAMILY = 'data'
+HBASE_METADATA_COLUMN_FAMILY = 'metadata'
+HBASE_PORT = 16010
+HOST = "10.4.41.48"
+
+# Configuration HDFS
+TMP_LANDING_DIR = "/temporal_landing"
+HDFS_PORT = "9870"
+HDFS_USER = "bdm"
+HDFS_DIRECTORIES = ['income', 'elections', 'idealista']
+
+def persist_to_hbase(table, row_key, data, schema):
+    table.put(row_key, {HBASE_COLUMN_FAMILY: data, HBASE_METADATA_COLUMN_FAMILY: schema})
+
+def read_from_hdfs(connection, directory):
+    table = connection.table(directory)
+
+    files = client.list(directory)
+    
+    for file in files:
+        with client.read(f'{directory}/{file}') as reader:
+            # read file
+            file_content = reader.read()
+            file_io = io.BytesIO(file_content)
+            table = pq.read_table(file_io)
+            
+            schema_str = table.schema.to_string()
+            data_dict = table.to_pandas().to_dict(orient='list')
+            
+            # key structure
+            row_key = f"{directory.split('/')[2]}${file}${int(time.time())}"
+
+            persist_to_hbase(table, row_key, data_dict, schema_str)
+
+if __name__ == "__main__":
+    # HBase connection
+    connection = happybase.Connection(HOST, HBASE_PORT)
+
+    # HDFS connection
+    client = InsecureClient(f'http://{HOST}:{HDFS_PORT}', user=HDFS_USER)
+
+    for directory in HDFS_DIRECTORIES:
+        if directory.encode() not in connection.tables():
+            connection.create_table(
+                directory,
+                {HBASE_COLUMN_FAMILY: dict()}
+            )
+            print(f"Table {directory} created in HBase.")
+
+        read_from_hdfs(connection, TMP_LANDING_DIR + '/' + directory)
